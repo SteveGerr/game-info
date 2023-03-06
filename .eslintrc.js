8f30ba40d1728eb70d9f11670c21dcb56b9e7039
SteveGerr <prikolsamara@mail.ru>
2023-03-06 19:32:16 +0400

    [feat] added configs, dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'@vue/airbnb',
+		'@vue/typescript/recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				devDependencies: false,
+				optionalDependencies: false,
+				peerDependencies: false,
+			},
+		],
+		'linebreak-style': 0,
+		indent: [1, 'tab'],
+		'no-tabs': 0,
+		'no-param-reassign': 0,
+		'no-explicit-any': 0,
+	},
 };
