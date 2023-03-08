65bbc22c09d23e4868b511375de4e0f0599b8f50
i.shunyaev <i.shunyaev@fact.digital>
2023-03-08 21:55:26 +0400

    [feat] added Games interface

---
+++ src/interfaces/GamesI.ts
@@ -0,0 +1,13 @@
+export interface GamesI {
+	id: number,
+	title: string
+	thumbnail: string,
+	short_description: string,
+	game_url: string,
+	genre: string,
+	platform: string,
+	publisher: string,
+	developer: string,
+	release_date: string,
+	profile_url: string
+}
