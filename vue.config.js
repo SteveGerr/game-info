48e1d3955c8ece810c26da7b1d32dbc7366212e9
i.shunyaev <i.shunyaev@fact.digital>
2023-03-10 10:43:44 +0400

    [feat] added loaderOptions to vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+	transpileDependencies: true,
+
+	css: {
+		loaderOptions: {
+			scss: {
+				// eslint-disable-next-line quotes
+				additionalData: `@import '@/assets/styles/index.scss';`,
+			},
+		},
+	},
 });
