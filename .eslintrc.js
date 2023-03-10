d31654b222be017a846bf87466e4743e50bbde72
i.shunyaev <i.shunyaev@fact.digital>
2023-03-10 10:42:51 +0400

    [refactor] changed eslintconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,18 +15,20 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'import/no-extraneous-dependencies': [
-			'error',
+			'off',
 			{
 				devDependencies: false,
 				optionalDependencies: false,
 				peerDependencies: false,
 			},
 		],
+		'import/prefer-default-export': 'off',
 		'linebreak-style': 0,
 		indent: [1, 'tab'],
 		camelcase: 'off',
 		'no-tabs': 0,
 		'no-param-reassign': 0,
 		'no-explicit-any': 0,
+		quotes: ['error', 'single'],
 	},
 };
