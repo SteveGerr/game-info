628da4851043996ea470d15633b7d901dd8093ac
i.shunyaev <i.shunyaev@fact.digital>
2023-03-11 20:55:08 +0400

    [feat] added interfaces: screenshots, SystemRequirementsI, GameI

--- src/interfaces/GamesI.ts
+++ src/interfaces/GamesI.ts
@@ -11,3 +11,33 @@ export interface GamesListI {
 	release_date: string,
 	profile_url: string
 }
+
+export interface ScreenshotsI {
+	id: number,
+	image: string
+}
+
+export interface SystemRequirementsI {
+	graphics: string,
+	memory: string,
+	os: string,
+	processor: string,
+	storage: string
+}
+
+export interface GameI {
+	id: number,
+	description: string,
+	title: string,
+	short_description: string,
+	game_url: string,
+	genre: string,
+	platform: string,
+	publisher: string,
+	developer: string,
+	release_date: string,
+	profile_url: string,
+	thumbnail: string,
+	screenshots: ScreenshotsI[]
+	minimum_system_requirements: SystemRequirementsI
+}
