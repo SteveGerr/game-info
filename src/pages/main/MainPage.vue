27f1e8f70be7254ccd9f70ca529f94cc68a33d28
i.shunyaev <i.shunyaev@fact.digital>
2023-03-11 20:57:31 +0400

    [feat] added main page

---
+++ src/pages/main/MainPage.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="main-page">
+		<h1 class="main-page__heading">Game finder</h1>
+		<ListGames class="list" />
+		<div class="filters"></div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import ListGames from '@/components/games-list/ListGames.vue';
+
+</script>
+
+<style lang="scss">
+.main-page {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: auto auto;
+	grid-template-areas:
+	"heading heading"
+	"filters list";
+	width: 100%;
+	max-width: 80vw;
+	height: 96vh;
+	margin: 1rem auto;
+	padding: 0 3rem;
+	gap: 2rem;
+	background-color: $secondary3;
+	border-radius: 5rem;
+
+	@include text-body;
+	color: $primary;
+
+	&__heading {
+		grid-area: heading;
+		@include text-h1;
+		color: $primary;
+	}
+
+	.list {
+		display: grid;
+		grid-area: list;
+		justify-content: flex-end;
+	}
+
+	.filters {
+		display: grid;
+		grid-area: filters;
+	}
+}
+</style>
