81feb7a23c61384fcefd39d41f1ab1efaec89c68
SteveGerr <prikolsamara@mail.ru>
2023-03-14 23:04:54 +0400

    [feat] added search

---
+++ src/components/search/AppSearch.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="search">
+		<span>Search</span>
+		<app-input :model-value="value" @change="change" />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { useGamesStore } from '@/stores/games';
+import { ref, Ref } from 'vue';
+import AppInput from '../common/inputs/AppInput.vue';
+
+const value: Ref<string> = ref('');
+
+const store = useGamesStore();
+
+// eslint-disable-next-line no-undef
+const change = (e: globalThis.Event) => {
+	const el = e.target as HTMLInputElement;
+	store.searchValue = el.value;
+};
+</script>
+
+<style lang="scss" scoped>
+.search {
+	display: flex;
+	flex-direction: column;
+}
+</style>
