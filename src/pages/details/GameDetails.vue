10f9b473a6ccba2f707f1f68bf7b8565340fe70e
SteveGerr <prikolsamara@mail.ru>
2023-03-14 23:55:27 +0400

    [refactor] changed layout

--- src/pages/details/GameDetails.vue
+++ src/pages/details/GameDetails.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="game-details">
 		<button-arrow class="game-details__back" />
-		<h1 class="game-details__heading">{{	store.gameDetails.title }}</h1>
+		<h1 class="game-details__heading">{{ store.gameDetails.title }}</h1>
 		<game-details-info :item="store.gameDetails" />
 		<div class="game-details__description" v-html="store.gameDetails.description"></div>
 		<system-requirements :item="store.gameDetails" />
@@ -27,32 +27,39 @@ getGame(gameId);
 </script>
 
 <style lang="scss" scoped>
-	.game-details {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		max-width: 80vw;
-		margin: 5rem auto;
-		position: relative;
+.game-details {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	max-width: 80vw;
+	margin: 5rem auto;
+	position: relative;
+	gap: 3rem;
 
-		&__back {
-			position: absolute;
-			top: 7rem;
-			left: -8rem;
-		}
-
-		&__heading {
-			width: 100%;
-			padding: 3rem;
-			@include text-h1;
-			color: $primary;
-			text-align: left;
-		}
-
-		&__description {
-			display: grid;
-			gap: 5rem;
-			padding: 3rem;
-		}
+	&__back {
+		position: absolute;
+		top: 7rem;
+		left: -8rem;
 	}
+
+	&__heading {
+		width: 100%;
+		padding: 3rem;
+		@include text-h1;
+		color: $primary;
+		font-family: 'CirceGlyphs';
+		text-align: left;
+		background-color: $gray-100-opacity;
+		border-radius: 0.8rem;
+	}
+
+	&__description {
+		display: grid;
+		gap: 5rem;
+		padding: 3rem;
+		background-color: $background-color;
+		border-radius: 0.8rem;
+		color: $gray-200;
+	}
+}
 </style>
