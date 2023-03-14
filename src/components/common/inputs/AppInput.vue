26b282625fed8d85614d8674a47753e9832ab4ec
SteveGerr <prikolsamara@mail.ru>
2023-03-14 23:04:25 +0400

    [feat] added custom input

---
+++ src/components/common/inputs/AppInput.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="app-input">
+		<label class="app-input__label" for="input">
+			<input
+				id="input"
+				class="app-input__input"
+				:value="modelValue"
+				@change="updateValue"
+				type="text"
+				placeholder="Enter the game"
+			/>
+		</label>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps<{
+	modelValue: string;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+
+// eslint-disable-next-line no-undef
+const updateValue = (e: globalThis.Event) => {
+	const el = e.target as HTMLInputElement;
+	emit('update:modelValue', el.value);
+};
+</script>
+
+<style lang="scss" scoped>
+.app-input {
+	display: flex;
+
+	&__label {
+		width: 100%;
+		border: 1px solid $gray-500;
+	}
+
+	&__input {
+		width: 100%;
+		padding: 1rem;
+		border: none;
+		outline: none;
+		background: $background-color;
+		@include text(1.8rem, 1.5rem, 600);
+		color: $gray-100;
+	}
+}
+</style>
