f5cf00182f77bb038a2e71d86930ed308099f307
SteveGerr <prikolsamara@mail.ru>
2023-03-14 23:13:54 +0400

    [feat] added search to store

--- src/stores/games.ts
+++ src/stores/games.ts
@@ -1,12 +1,23 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable no-return-assign */
+/* eslint-disable implicit-arrow-linebreak */
 import { defineStore } from 'pinia';
-import { ref, Ref } from 'vue';
+import { computed, ref, Ref } from 'vue';
 import { GamesListI, GameI } from '../interfaces/GamesI';
 import GamesApi from '../api/GamesApi';
 
 export const useGamesStore = defineStore('games', () => {
 	const gamesList: Ref<GamesListI[]> = ref([]);
 	const gameDetails: Ref<GameI> = ref({} as GameI);
+	const searchValue: Ref<string> = ref('');
+
+	const filterGameByName = computed(() => {
+		const result = gamesList.value.filter(
+			(game) => game.title.toLowerCase().includes(searchValue.value.toLowerCase())
+			// eslint-disable-next-line function-paren-newline
+		);
+		return result;
+	});
 
 	const getGames = async () => {
 		try {
@@ -30,7 +41,9 @@ export const useGamesStore = defineStore('games', () => {
 	};
 
 	return {
+		filterGameByName,
 		getGameDetails,
+		searchValue,
 		gameDetails,
 		gamesList,
 		getGames,
