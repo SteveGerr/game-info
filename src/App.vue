29ce265c199894eb3a72a33e6f3f63dc1bb10b9a
SteveGerr <prikolsamara@mail.ru>
2023-03-14 23:11:05 +0400

    [feat] added background

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="games-app">
-		<RouterView/>
+		<RouterView />
 	</div>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss">
 .games-app {
 	display: flex;
 	justify-content: space-between;
+	min-height: 100vh;
+	background: url('assets/img/bg.webp');
+	background-size: cover;
+	background-repeat: no-repeat;
 
 	@include text-body;
 	color: $gray-400;
