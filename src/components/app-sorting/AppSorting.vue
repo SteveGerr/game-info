038db80c33b9c57a081fdf2ff46f1ba6ab0d0ef7
SteveGerr <prikolsamara@mail.ru>
2023-03-18 07:43:41 +0400

    [feat] added select component

---
+++ src/components/app-sorting/AppSorting.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="sorting">
+		<p>Sorting</p>
+		<app-select />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import AppSelect from '@/components/common/select/AppSelect.vue';
+</script>
+
+<style lang="scss" scoped></style>
