0ec6f94f497d0faac7262a08dcb5a918afae7eef
i.shunyaev <i.shunyaev@fact.digital>
2023-03-19 23:24:43 +0400

    [refactor] changed app select attr

--- src/components/app-sorting/AppSorting.vue
+++ src/components/app-sorting/AppSorting.vue
@@ -1,12 +1,19 @@
 <template>
 	<div class="sorting">
 		<p>Sorting</p>
-		<app-select />
+		<app-select
+			:value="store.selectValue"
+			:options="store.gamesCategory"
+			@update:model-value="(e) => store.sortByCategory(e)"
+		/>
 	</div>
 </template>
 
 <script lang="ts" setup>
 import AppSelect from '@/components/common/select/AppSelect.vue';
+import { useGamesStore } from '@/stores/games';
+
+const store = useGamesStore();
 </script>
 
 <style lang="scss" scoped></style>
